c9a2e83db72856d249d2c66f15260b29adbb4556
Vvvika98 <petrusha.sv@yandex.ru>
2025-03-15 23:05:53 +0300

    second commit

---
+++ Python_from_0/learn4.py
@@ -0,0 +1,37 @@
+# БУЛЕВЫЙ ТИП ДАННЫХ 
+# is_student = True
+# is_graduated = False
+# print(is_graduated)
+
+
+x = 9
+y = 10
+print(x == y)
+# сравнение в Python делается в помощь ""=="" - для проверки равенства переменных
+
+x = 10
+y = 10
+is_equal = x == y 
+# задаем переменную 
+print(is_equal)
+
+
+x = 11
+y = 10
+not_equal = x != y 
+print(not_equal)
+# оператор для проверки неравенства переменных 
+
+x = 10
+y = 9 
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 1
+print(x < 5 and x > -2)
+# and или or чтобы проверить два условия
+
+# not меняет знак у переменной типа bool
+

aaaff223d6a56e2225c7819004e19b656ce9244b
Vvvika98 <petrusha.sv@yandex.ru>
2025-03-23 00:10:00 +0300

    решила задание от Сани

--- Python_from_0/homework3.py
+++ Python_from_0/homework3.py
@@ -6,13 +6,13 @@
 # 9 * 9 = 81
 
 # решение 
-numbers = range(1, 10)
-numbers2 = range(1, 10)
-for num in numbers:
+numbers = range(1, 10) #создаем переменную 1 с последовательностью чисел 
+numbers2 = range(1, 10) #создаем переменную 2 с последовательностью чисел 
+for num in numbers: # цикл 1
     # print(num)
-    for num2 in numbers2:
+    for num2 in numbers2: # цикл 2
         # print(num, num2)
         result = num * num2 
         # print(result)
-        print(f"{num} * {num2} = {result}") # 
+        print(f"{num} * {num2} = {result}") # в ф-строку можно вписывать любые значения, а переменные брать в {}
 

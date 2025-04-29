422fe56b17306bf39d57653063ff30fe254aca48
Vvvika98 <petrusha.sv@yandex.ru>
2025-04-29 22:24:02 +0300

    little_sisters

--- exercism/python/little-sisters-vocab/strings.py
+++ exercism/python/little-sisters-vocab/strings.py
@@ -7,11 +7,11 @@ def add_prefix_un(word):
     :param word: str - containing the root word.
     :return: str - of root word prepended with 'un'.
     """
-    word_2 = "un"
-    return word_2 + word 
 
-print(add_prefix_un("happy"))
-print(add_prefix_un("manageable"))
+    return f"un{word}"
+
+# print(add_prefix_un("happy"))
+# print(add_prefix_un("manageable"))
 
 
 def make_word_groups(vocab_words):
@@ -29,13 +29,20 @@ def make_word_groups(vocab_words):
     produces the following string: 'en :: enclose :: enjoy :: enlighten'.
     """
     
-    prefixes = vocab_words[0] #вычисляем префикс 
-    zero_list = [prefixes] #создаем "пустой" список и вначале добавляем префикс
-    for index in range(1, len(vocab_words)): #запускаем цикл for, в котором берем слова в интревале 1-4 по len? range дает 1-3
-        result = prefixes + vocab_words[index] #добавляем в каждому слову по индексу из цикла префикс и присваиваем это в переменную 
-        zero_list.append(result) #используя метод добавляются полученные слова из цикла (по одному за каждый цикл)
+    # prefixes = vocab_words[0] #вычисляем префикс 
+    # zero_list = [prefixes] #создаем "пустой" список и вначале добавляем префикс
+    # for index in range(1, len(vocab_words)): #запускаем цикл for, в котором берем слова в интревале 1-4 по len? range дает 1-3
+    #     result = prefixes + vocab_words[index] #добавляем в каждому слову по индексу из цикла префикс и присваиваем это в переменную 
+    #     zero_list.append(result) #используя метод добавляются полученные слова из цикла (по одному за каждый цикл)
 
-    return ' :: '.join(zero_list) #используя метод добавляем разделитель в список, преобразуя его в строку с разделителем 
+    # return ' :: '.join(zero_list) #используя метод добавляем разделитель в список, преобразуя его в строку с разделителем 
+
+    prefix = vocab_words[0] #вычисляем префикс 
+    result = [prefix] #пустой список для результата
+    for word in vocab_words[1:]:
+        result.append(f"{prefix}{word}")
+    return ' :: '.join(result)
+
 
 print(make_word_groups(['en', 'close', 'joy', 'lighten']))
 print(make_word_groups(['pre', 'serve', 'dispose', 'position']))
@@ -58,7 +65,15 @@ def remove_suffix_ness(word):
     else:
         return word
     print(word)
-print(remove_suffix_ness("heaviness"))
+
+    
+    word = word[:-4]
+    if word.endswith("i"): #новый метод 
+        return word[:-1] + "y"
+    return word
+    print(word)
+
+# print(remove_suffix_ness("heaviness"))
 
 
 def adjective_to_verb(sentence, index):
@@ -71,4 +86,10 @@ def adjective_to_verb(sentence, index):
     For example, ("It got dark as the sun set.", 2) becomes "darken".
     """
 
-    pass
+    
+    return sentence.strip(".").split()[index] + "en" #можно применять несколько методов одновременно 
+    
+    
+print(adjective_to_verb('I need to make that bright.', -1))  
+
+print(adjective_to_verb('It got dark as the sun set.', 2))  

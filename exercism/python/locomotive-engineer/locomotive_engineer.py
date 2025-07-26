4d1f69aef09ea933b76301c80597dd42297fd1b9
Vvvika98 <petrusha.sv@yandex.ru>
2025-07-26 19:50:51 +0300

    corr

--- exercism/python/locomotive-engineer/locomotive_engineer.py
+++ exercism/python/locomotive-engineer/locomotive_engineer.py
@@ -23,15 +23,13 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :param missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    a,b, *rest = each_wagons_id
-    vagon, *lost = rest
-    result = [vagon, *missing_wagons, *lost, a,b]
-    return result
+    first,second, locomotive, *rest = each_wagons_id
+    return [locomotive, *missing_wagons, *rest, first,second]
     
 # print(fix_list_of_wagons([2, 5, 1, 7, 4, 12, 6, 3, 13], [3, 17, 6, 15]))
 
 
-def add_missing_stops(route, **kwargs):
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
@@ -39,15 +37,15 @@ def add_missing_stops(route, **kwargs):
     :return: dict - updated route dictionary.
     """
 
-    stops = (kwargs)
-    st = [value for value in stops.values()]
-    full_stops = dict(stops = st)
-    result = {**route, **full_stops}
-    return result
+    # st = [value for value in stops.values()]
+    # full_stops = dict(stops = st)
+    # return {**route, **full_stops}
+
+    return {**route, "stops": list(stops.values())}
 
 # print(add_missing_stops({"from": "New York", "to": "Miami"},
-                    #   stop_1="Washington, DC", stop_2="Charlotte", stop_3="Atlanta",
-                    #   stop_4="Jacksonville", stop_5="Orlando"))
+#                       stop_1="Washington, DC", stop_2="Charlotte", stop_3="Atlanta",
+#                       stop_4="Jacksonville", stop_5="Orlando"))
 
 
 
@@ -58,8 +56,8 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    result = {**route, **more_route_information}
-    return result
+  
+    return {**route, **more_route_information}
 
 # print(extend_route_information({"from": "Berlin", "to": "Hamburg"}, {"length": "100", "speed": "50"}))
 
@@ -69,10 +67,12 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    a, b, c = zip(*wagons_rows)             #с помощью зип возвращается итератор кортежей 1х элементов, 2х, 3х и т.д.
-    a,b,c = list(a), list(b), list(c)
-    result = [a,b,c]
-    return result
+    # a, b, c = zip(*wagons_rows)             #с помощью зип возвращается итератор кортежей 1х элементов, 2х, 3х и т.д.
+    # a,b,c = list(a), list(b), list(c)
+    # result = [a,b,c]
+    # return result
+
+    return [list(i) for i in zip(*wagons_rows)]
 
 print(fix_wagon_depot([
                     [(2, "red"), (4, "red"), (8, "red")],

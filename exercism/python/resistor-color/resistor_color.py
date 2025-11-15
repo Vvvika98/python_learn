55179283268dbe0cb95917946a981933161b81c6
Vvvika98 <petrusha.sv@yandex.ru>
2025-11-16 00:03:56 +0300

    full

---
+++ exercism/python/resistor-color/resistor_color.py
@@ -0,0 +1,27 @@
+RESISTORS = { 
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+def color_code(color):
+    return RESISTORS[color]
+
+
+def colors():
+    return [color for color in RESISTORS.keys()]
+
+
+
+
+print(color_code("orange"))
+
+print(colors())
+

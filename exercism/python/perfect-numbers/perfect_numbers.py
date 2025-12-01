6bd96ff4d2761b2352d1b40518291edf0dd59a47
Vvvika98 <petrusha.sv@yandex.ru>
2025-12-01 21:09:17 +0300

    correct_v

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -4,7 +4,7 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    aliquot_sum = sum([num for num in range(1, number) if number % num == 0])
+    aliquot_sum = sum(num for num in range(1, number) if number % num == 0) #здесь ЛК [] не нужен даже т.к. нам важна сама сумма
     
     if number <= 0:
         raise ValueError("Classification is only possible for positive integers.")
@@ -20,7 +20,7 @@ def classify(number):
 
 # print(classify(6))  #"perfect"
 
-# print(classify(12)) #"abundant"
+print(classify(12)) #"abundant"
 
 # print(classify(8))  #"deficient"
 
